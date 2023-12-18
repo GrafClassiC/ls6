93c24dd779b6845634302901e3103164f782b1e2
Graf <vlasov.art.ser@yandex.ru>
2023-12-18 19:21:05 +0400

    Частичное решение дз 6

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+/*Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, 
+в которой все заглавные буквы заменены на строчные.
+*/
+/*
+string text = "ShivoroT na VivOroT";
+            string newtext = text.ToUpper();
+            MessageBox.Show(newtext);
+            */
+
+
+
+/*Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.*/
+
+/* using System;
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = { 'p', 'a', 's', 'a', 'p'};
+            if (IsPalindrom(arr))
+                Console.WriteLine("АгА");
+            else
+                Console.WriteLine("Неа");
+        }
+ 
